e37c1292dafb5eb28937fb4278343947aae09cfa
liyue <1004214588@qq.com>
2022-06-14 10:54:24 +0800

    2022-06-14

---
+++ 72.编辑距离.js
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode.cn id=72 lang=javascript
+ *
+ * [72] 编辑距离
+ */
+
+// @lc code=start
+/**
+ * @param {string} word1
+ * @param {string} word2
+ * @return {number}
+ */
+var minDistance = function (word1, word2) {
+  let m = word1.length, n = word2.length;
+  let dp = new Array(m + 1);
+  for (let i = 0; i <= m; i++) {
+    dp[i] = new Array(n + 1);
+    dp[i][0] = i;
+  }
+  for (let j = 1; j <= n; j++) {
+    dp[0][j] = j;
+  }
+  for (let i = 1; i <= m; i++) {
+    for (let j = 1; j <= n; j++) {
+      if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+        dp[i][j] = dp[i - 1][j - 1];
+      } else {
+        dp[i][j] = Math.min(dp[i - 1][j], Math.min(dp[i][j - 1], dp[i - 1][j - 1])) + 1;
+      }
+    }
+  }
+  return dp[m][n];
+};
+// @lc code=end
+
--- 89.格雷编码.js
+++ 89.格雷编码.js
@@ -9,8 +9,19 @@
  * @param {number} n
  * @return {number[]}
  */
-var grayCode = function(n) {
-
+var grayCode = function (n) {
+  let ret = new Array(1 << n);
+  if (n == 0) {
+    ret[0] = 0;
+    return ret;
+  }
+  let code_n_1 = grayCode(n - 1);
+  let len_n_1 = code_n_1.length;
+  for (let i = 0; i < len_n_1; i++){
+    ret[i] = code_n_1[i] << 1;
+    ret[2 * len_n_1 - i - 1] = code_n_1[i] << 1 | 1;
+  }
+  return ret;
 };
 // @lc code=end
 

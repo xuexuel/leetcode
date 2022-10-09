01d98d43062a2467e074f66c48132c72cba74bb0
liyue <1004214588@qq.com>
2022-10-09 16:15:22 +0800

    2020-10-08

--- 55.跳跃游戏.js
+++ 55.跳跃游戏.js
@@ -9,10 +9,10 @@
  * @param {number[]} nums
  * @return {boolean}
  */
-var canJump = function(nums) {
+var canJump = function (nums) {
   let n = nums.length;
   let farthest = 0;
-  for (let i = 0; i < n-1; i++) {
+  for (let i = 0; i < n - 1; i++) {
     farthest = Math.max(farthest, i + nums[i]);
     if (farthest <= i) return false;
   }
